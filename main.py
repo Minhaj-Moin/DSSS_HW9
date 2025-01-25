04cfbda76558d14b597ca9384900c11a543adc49
Minhaj Ahmed Moin <31467353+Minhaj-Moin@users.noreply.github.com>
2025-01-25 01:43:16 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+## TO RUN ON GOOGLE COLAB
+# !pip install -q python-telegram-bot
+from telegram import Update
+import requests
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+
+# from google.colab import userdata
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! MinhajBot here, how can I help you?")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    json_data = {'contents': [{'parts': [{'text': update.message.text}]}]}
+
+    response = requests.post(
+        'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent',
+        params={'key': userdata.get('GOOGLE_API_KEY')},
+        headers={'Content-Type': 'application/json'},
+        json=json_data)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response.json()['candidates'][0]['content']['parts'][0]['text'])
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(userdata.get('TELEGRAM_API_KEY')).build()
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    
+    application.run_polling()
