5bf26959e4a8c09e0800ffebbf0bb04dda7ca531
Minhaj Ahmed Moin <31467353+Minhaj-Moin@users.noreply.github.com>
2025-01-25 02:00:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 #from google.colab import userdata
 import os
-print(os.environ['TELEGRAM_API_KEY'])
+# print(os.environ['TELEGRAM_API_KEY'])
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! MinhajBot here, how can I help you?")
@@ -15,14 +15,14 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     response = requests.post(
         'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent',
-        params={'key': userdata.get('GOOGLE_API_KEY')},
+        params={'key': os.environ['GOOGLE_API_KEY']},
         headers={'Content-Type': 'application/json'},
         json=json_data)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response.json()['candidates'][0]['content']['parts'][0]['text'])
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(userdata.get('TELEGRAM_API_KEY')).build()
+    application = ApplicationBuilder().token(os.environ['TELEGRAM_API_KEY']).build()
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
